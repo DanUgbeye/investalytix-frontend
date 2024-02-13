ce5c4045dcc00bfd786a4dc27e0149487c1d54d7
Daniel Ugbeye <ugbeyellionz@gmail.com>
2024-02-13 16:55:22 +0100

    additional setups

---
+++ src/app/fonts.ts
@@ -0,0 +1,7 @@
+import localFont from "next/font/local";
+import { DM_Sans, Inter } from "next/font/google";
+
+export const interFont = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { interFont } from "./fonts";
 
 export const metadata: Metadata = {
   title: "Investalytix",
@@ -16,7 +14,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={` ${interFont.variable} `}>{children}</body>
     </html>
   );
 }
+
