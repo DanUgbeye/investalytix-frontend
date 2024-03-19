35940be22a86dd3fae757b228462bf7928a237ef
Daniel Ugbeye <ugbeyellionz@gmail.com>
2024-03-19 17:16:07 +0100

    ratio screen done

--- src/app/ticker/[ticker]/stock-description/ratios/screen.tsx
+++ src/app/ticker/[ticker]/stock-description/ratios/screen.tsx
@@ -9,6 +9,7 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
+import RatioTable from "./ratio-table";
 
 interface StockDescriptionRatioScreenProps {
   ticker: string;
@@ -20,22 +21,270 @@ export default function StockDescriptionRatioScreen(
   const { ticker } = props;
 
   return (
-    <section className="  ">
+    <section className=" space-y-8  ">
       <div className=" grid divide-y lg:grid-cols-3 lg:divide-x lg:divide-y-0 ">
-        <div className=" flex lg:px-3 flex-wrap gap-x-4 py-2 lg:w-full lg:justify-center ">
+        <div className=" flex flex-wrap gap-x-4 py-2 lg:w-full lg:justify-center lg:px-3 ">
           <span className="  ">Fiscal Year End</span>
           <span className=" font-bold ">09/2023</span>
         </div>
 
-        <div className=" flex lg:px-3 flex-wrap gap-x-4 py-2 lg:w-full lg:justify-center ">
+        <div className=" flex flex-wrap gap-x-4 py-2 lg:w-full lg:justify-center lg:px-3 ">
           <span className="  ">Last Quarter End</span>
           <span className=" font-bold ">09/2023 Q4</span>
         </div>
 
-        <div className=" flex lg:px-3 flex-wrap gap-x-4 py-2 lg:w-full lg:justify-center ">
+        <div className=" flex flex-wrap gap-x-4 py-2 lg:w-full lg:justify-center lg:px-3 ">
           <span className="  ">Current/T12M</span>
           <span className=" font-bold ">(USD)</span>
         </div>
+      </div>
+
+      <div className=" grid sm:justify-center gap-10 sm:grid-cols-2 xl:grid-cols-3 ">
+        <RatioTable
+          className="  "
+          ratio={{
+            name: "Issue Data",
+            fields: [
+              {
+                label: "Last Px",
+                value: "USD/182.89",
+              },
+              {
+                label: "P/E",
+                value: "29.9",
+              },
+              {
+                label: "Dvd Ind Yld",
+                value: "0.5%",
+              },
+              {
+                label: "P/B",
+                value: "17.69",
+              },
+              {
+                label: "P/S",
+                value: "17.69",
+              },
+              {
+                label: "52 Week Range",
+                value: "3.11 - 20.09",
+              },
+              {
+                label: "Volume",
+                value: "59,763,221",
+              },
+              {
+                label: "Avg. Volume",
+                value: "40,368,106",
+              },
+            ],
+          }}
+        />
+
+        <RatioTable
+          className="  "
+          ratio={{
+            name: "Per Share Data",
+            fields: [
+              {
+                label: "Last Px",
+                value: "USD/182.89",
+              },
+              {
+                label: "P/E",
+                value: "29.9",
+              },
+              {
+                label: "Dvd Ind Yld",
+                value: "0.5%",
+              },
+              {
+                label: "P/B",
+                value: "17.69",
+              },
+              {
+                label: "P/S",
+                value: "17.69",
+              },
+              {
+                label: "52 Week Range",
+                value: "3.11 - 20.09",
+              },
+              {
+                label: "Volume",
+                value: "59,763,221",
+              },
+              {
+                label: "Avg. Volume",
+                value: "40,368,106",
+              },
+            ],
+          }}
+        />
+
+        <RatioTable
+          className="  "
+          ratio={{
+            name: "Cash Flow Analysis",
+            fields: [
+              {
+                label: "Last Px",
+                value: "USD/182.89",
+              },
+              {
+                label: "P/E",
+                value: "29.9",
+              },
+              {
+                label: "Dvd Ind Yld",
+                value: "0.5%",
+              },
+              {
+                label: "P/B",
+                value: "17.69",
+              },
+              {
+                label: "P/S",
+                value: "17.69",
+              },
+              {
+                label: "52 Week Range",
+                value: "3.11 - 20.09",
+              },
+              {
+                label: "Volume",
+                value: "59,763,221",
+              },
+              {
+                label: "Avg. Volume",
+                value: "40,368,106",
+              },
+            ],
+          }}
+        />
+
+        <RatioTable
+          className="  "
+          ratio={{
+            name: "Growth Potential",
+            fields: [
+              {
+                label: "Last Px",
+                value: "USD/182.89",
+              },
+              {
+                label: "P/E",
+                value: "29.9",
+              },
+              {
+                label: "Dvd Ind Yld",
+                value: "0.5%",
+              },
+              {
+                label: "P/B",
+                value: "17.69",
+              },
+              {
+                label: "P/S",
+                value: "17.69",
+              },
+              {
+                label: "52 Week Range",
+                value: "3.11 - 20.09",
+              },
+              {
+                label: "Volume",
+                value: "59,763,221",
+              },
+              {
+                label: "Avg. Volume",
+                value: "40,368,106",
+              },
+            ],
+          }}
+        />
+
+        <RatioTable
+          className="  "
+          ratio={{
+            name: "Profitability",
+            fields: [
+              {
+                label: "Last Px",
+                value: "USD/182.89",
+              },
+              {
+                label: "P/E",
+                value: "29.9",
+              },
+              {
+                label: "Dvd Ind Yld",
+                value: "0.5%",
+              },
+              {
+                label: "P/B",
+                value: "17.69",
+              },
+              {
+                label: "P/S",
+                value: "17.69",
+              },
+              {
+                label: "52 Week Range",
+                value: "3.11 - 20.09",
+              },
+              {
+                label: "Volume",
+                value: "59,763,221",
+              },
+              {
+                label: "Avg. Volume",
+                value: "40,368,106",
+              },
+            ],
+          }}
+        />
+
+        <RatioTable
+          className="  "
+          ratio={{
+            name: "Structure",
+            fields: [
+              {
+                label: "Last Px",
+                value: "USD/182.89",
+              },
+              {
+                label: "P/E",
+                value: "29.9",
+              },
+              {
+                label: "Dvd Ind Yld",
+                value: "0.5%",
+              },
+              {
+                label: "P/B",
+                value: "17.69",
+              },
+              {
+                label: "P/S",
+                value: "17.69",
+              },
+              {
+                label: "52 Week Range",
+                value: "3.11 - 20.09",
+              },
+              {
+                label: "Volume",
+                value: "59,763,221",
+              },
+              {
+                label: "Avg. Volume",
+                value: "40,368,106",
+              },
+            ],
+          }}
+        />
       </div>
     </section>
   );
