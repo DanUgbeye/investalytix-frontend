314d07ae90763938e12db80b8f181ed62d294e5d
wisdomose <wisdomose05@gmail.com>
2024-03-31 06:54:37 +0100

    fix: icons

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -11,7 +11,13 @@ import {
   ReactPropTypes,
   useState,
 } from "react";
-import { FiArrowLeft, FiChevronRight, FiX } from "react-icons/fi";
+import {
+  FiArrowLeft,
+  FiChevronRight,
+  FiMoon,
+  FiSun,
+  FiX,
+} from "react-icons/fi";
 import useTheme from "@/store/theme/useTheme";
 
 type RouteLink = { label: string; children?: RouteLink[]; href: string };
@@ -60,7 +66,7 @@ const routes: RouteLink[] = [
 ];
 
 export default function NavBar() {
-  const { toggleTheme } = useTheme();
+  const { toggleTheme, theme } = useTheme();
   return (
     <nav className="bg-[#0B0B0B] py-3">
       <Container>
@@ -95,10 +101,11 @@ export default function NavBar() {
           <div className="flex items-center">
             <Search />
             <button
-              className="ml-6 inline-block py-4 font-bold text-white"
+              className="ml-6 inline-block font-bold text-white rounded-full p-2"
               onClick={toggleTheme}
             >
-              <svg
+              {theme === "light" ? <FiMoon /> : <FiSun />}
+              {/* <svg
                 width={20}
                 height={20}
                 viewBox="0 0 32 32"
@@ -109,7 +116,7 @@ export default function NavBar() {
                   d="M13.3346 9.33268C13.3344 11.1869 13.8864 12.9991 14.9203 14.5383C15.9542 16.0775 17.4231 17.2738 19.1397 17.9748C20.8563 18.6757 22.7427 18.8495 24.5585 18.4739C26.3742 18.0983 28.037 17.1904 29.3346 15.866V15.9993C29.3346 23.3633 23.3653 29.3327 16.0013 29.3327C8.6373 29.3327 2.66797 23.3633 2.66797 15.9993C2.66797 8.63535 8.6373 2.66602 16.0013 2.66602H16.1346C15.2467 3.53418 14.5415 4.5713 14.0606 5.71626C13.5797 6.86121 13.3329 8.09084 13.3346 9.33268ZM5.33464 15.9993C5.33367 18.3793 6.12867 20.6913 7.59316 22.5673C9.05764 24.4433 11.1075 25.7757 13.4165 26.3524C15.7256 26.9292 18.1611 26.7171 20.3358 25.75C22.5104 24.7829 24.2991 23.1163 25.4173 21.0154C23.4272 21.4842 21.3503 21.4368 19.3837 20.8776C17.4171 20.3185 15.626 19.2661 14.1803 17.8204C12.7345 16.3747 11.6822 14.5835 11.123 12.6169C10.5639 10.6503 10.5165 8.57343 10.9853 6.58335C9.27807 7.49287 7.85035 8.84957 6.85498 10.5082C5.85961 12.1668 5.33407 14.065 5.33464 15.9993Z"
                   fill="white"
                 />
-              </svg>
+              </svg> */}
             </button>
             <button className="hidden rounded bg-transparent px-8 py-2 font-bold text-white md:block">
               Login
@@ -266,7 +273,7 @@ function Search() {
   return (
     <>
       <button
-        className="grid place-content-center overflow-hidden rounded-full bg-[#262626] p-3"
+        className="grid place-content-center overflow-hidden rounded-full p-2"
         onClick={toggleIsOpen}
       >
         <svg
