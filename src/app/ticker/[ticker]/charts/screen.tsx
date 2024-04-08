13e9bfd82719a4d4ddcb49a8bf6b6faf4f20c598
Daniel Ugbeye <ugbeyellionz@gmail.com>
2024-04-08 08:07:46 +0100

    charts page done

--- src/app/ticker/[ticker]/charts/screen.tsx
+++ src/app/ticker/[ticker]/charts/screen.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Container } from "@/components/container";
+import useTheme from "@/store/theme/useTheme";
+import { useEffect, useRef } from "react";
 
 interface ChartsScreenProps {
   ticker: string;
@@ -9,5 +10,43 @@ interface ChartsScreenProps {
 export default function ChartsScreen(props: ChartsScreenProps) {
   const { ticker } = props;
 
-  return <Container className="  "></Container>;
+  const container = useRef<HTMLDivElement>(null);
+  const { theme } = useTheme();
+
+  useEffect(() => {
+    const ref = container.current;
+    if (ref === null) return;
+
+    while (ref.firstChild) {
+      ref.removeChild(ref.firstChild);
+    }
+    const script = document.createElement("script");
+    script.src =
+      "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+    script.type = "text/javascript";
+    script.async = true;
+    script.innerHTML = `
+        {
+          "autosize": true,
+          "symbol": "${ticker}",
+          "interval": "D",
+          "timezone": "Etc/UTC",
+          "theme": "${theme}",
+          "style": "1",
+          "locale": "en",
+          "enable_publishing": false,
+          "allow_symbol_change": true,
+          "calendar": false,
+          "support_host": "https://www.tradingview.com"
+        }`;
+    ref.appendChild(script);
+  }, [theme, ticker]);
+
+  return (
+    <div
+      className=" relative h-[calc(100vh_-_176px)] w-full overflow-hidden py-10"
+      ref={container}
+    />
+  );
+
 }
