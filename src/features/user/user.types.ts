f6d461dfd4eba380c8eda2c66ca1b8c8080a3d42
Daniel Ugbeye <ugbeyellionz@gmail.com>
2024-04-14 16:04:54 +0100

    add user validation

--- src/features/user/user.types.ts
+++ src/features/user/user.types.ts
@@ -1,4 +1,12 @@
-export type User = {
+export type ServerUserData = {
+  _id: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+  googleId?: string;
+};
+
+export type UserData = {
   id: string;
   email: string;
   firstname: string;
