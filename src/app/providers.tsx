4811c33b3ed440e9f6c412becb3e21cb3f7793b1
Daniel Ugbeye <ugbeyellionz@gmail.com>
2024-04-16 06:41:55 +0100

    create QUERY_KEYS and LOCALSTOARGE_KEYS constants

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,12 +2,17 @@
 
 import AuthProvider from "@/modules/auth/components/auth-provider";
 import ThemeContextProvider from "@/store/theme/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
 
 export default function Providers({ children }: PropsWithChildren) {
+  const queryClient = new QueryClient();
+
   return (
-    <AuthProvider>
-      <ThemeContextProvider>{children}</ThemeContextProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <ThemeContextProvider>{children}</ThemeContextProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   );
 }
---
+++ src/data/query-keys.ts
@@ -0,0 +1,3 @@
+export const QUERY_KEYS = {
+  GET_USER_PROFILE: "GET_USER_PROFILE",
+} as const;
