21f53b766be33dbd03ba6d1f598252ec47016566
Daniel Ugbeye <ugbeyellionz@gmail.com>
2024-04-16 06:41:07 +0100

    add getUserProfile method to user repo and create 'useUserRepo' hook

--- src/modules/user/repository/index.ts
+++ src/modules/user/repository/index.ts
@@ -1,6 +1,38 @@
+import { clientAPI } from "@/config/api";
+import { RequestOptions } from "@/types/api.types";
+import { handleAPIError } from "@/utils/api-utils";
+import { AxiosInstance } from "axios";
+import { UserData } from "../user.types";
+import { UserSchema } from "../validation";
+
 export class UserRepository {
-  constructor() {}
+  constructor(private api: AxiosInstance) {}
+
+  async getUserProfile(userId: string, options?: RequestOptions) {
+    const path = `/users/${userId}`;
+
+    try {
+      const { data } = await this.api.get<{ data: { User: UserData } }>(
+        path,
+        options
+      );
+      let parsedRes = UserSchema.safeParse(data.data.User);
+
+      if (!parsedRes.success) {
+        throw new Error("Something went wrong on our end");
+      }
+
+      return parsedRes.data;
+    } catch (error: any) {
+      let err = handleAPIError(error);
+      throw err;
+    }
+  }
 
   updateUser() {}
   updateProfileImage() {}
+}
+
+export function useUserRepo() {
+  return new UserRepository(clientAPI);
 }
