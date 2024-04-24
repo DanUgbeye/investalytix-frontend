4dc3a2b68f42c35f1b8804734e0c32e9910ac135
wisdomose <wisdomose05@gmail.com>
2024-04-24 07:07:07 +0100

    feat: persist theme across page refresh

--- src/store/theme/store.tsx
+++ src/store/theme/store.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { createContext, useCallback, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const ThemeContext = createContext({
   theme: "light",
@@ -14,14 +14,23 @@ export default function ThemeContextProvider({
   const [theme, setTheme] = useState<"light" | "dark">("light");
 
   const toggleTheme = () => {
+    setTheme((theme) => (theme === "light" ? "dark" : "light"));
+    // persist theme in local storage
+    localStorage.setItem("theme", theme === "light" ? "dark" : "light");
+  };
+
+  useEffect(() => {
     const body = document.querySelector("body");
     if (!body) return;
-
-    setTheme((theme) => (theme === "light" ? "dark" : "light"));
-
     body.classList.toggle("dark");
-  };
-  
+  }, [theme]);
+
+  // initialize theme from local storage
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    setTheme((theme as any) ?? "light");
+  }, []);
+
   const value = { theme, toggleTheme };
   return (
     <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
