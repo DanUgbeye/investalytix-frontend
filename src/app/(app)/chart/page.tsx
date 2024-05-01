3f17141057258f3a669eb973d63d3c546dad7b90
wisdomose <wisdomose05@gmail.com>
2024-05-01 03:52:36 +0100

    feat: watchlist

--- src/app/(app)/chart/page.tsx
+++ src/app/(app)/chart/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { Container } from "@/components/container";
 import { Time, createChart } from "lightweight-charts";
-import { memo, useEffect, useRef } from "react";
+import { Fragment, memo, useEffect, useRef } from "react";
 import chartData from "@/mock/chart";
 import useTheme from "@/store/theme/useTheme";
+import { Tab } from "@headlessui/react";
+import { FiLock } from "react-icons/fi";
 
 function ChartPage() {
   const container = useRef<HTMLDivElement>(null);
@@ -38,23 +40,73 @@ function ChartPage() {
   }, [theme]);
 
   return (
-    <div
-      className=" relative h-[calc(100vh_-_176px)] w-full overflow-hidden"
-      ref={container}
-    >
-      {/* <div
+    <div className="grid lg:grid-cols-[1fr,300px]">
+      <div
+        className=" relative h-[calc(100vh_-_144px)] w-full overflow-hidden md:h-[calc(100vh_-_148px)]"
+        ref={container}
+      >
+        {/* <div
         className="tradingview-widget-container__widget"
         style={{ height: "calc(100% - 32px)", width: "100%" }}
-      ></div>
+        ></div>
       <div className="tradingview-widget-copyright">
         <a
           href="https://www.tradingview.com/"
           rel="noopener nofollow"
           target="_blank"
         >
-          <span className="blue-text">Track all markets on TradingView</span>
+        <span className="blue-text">Track all markets on TradingView</span>
         </a>
       </div> */}
+      </div>
+      <div className="grid w-full grid-rows-[auto,1fr] gap-2 overflow-hidden border-l pl-2 pt-2 max-lg:hidden border-l-black dark:border-l-white/10 resize">
+        <h1 className="text-lg font-semibold capitalize">my list</h1>
+
+        <Tab.Group>
+          <div className="grid grid-rows-[auto,1fr] overflow-hidden">
+            <Tab.List className={"grid grid-cols-2 gap-5"}>
+              <Tab as={Fragment}>
+                {({ selected }) => (
+                  <button
+                    className={`white-text text-hover-focus flex items-center justify-center gap-3 border-b-2 px-2 pb-2 text-sm outline-none ${
+                      selected
+                        ? "border-primary-base dark:border-primary-light"
+                        : "border-transparent"
+                    }`}
+                  >
+                    Portfolio <FiLock className="size-3" />
+                  </button>
+                )}
+              </Tab>
+              <Tab as={Fragment}>
+                {({ selected }) => (
+                  <button
+                    className={`white-text text-hover-focus border-b-2 px-2 pb-2 text-sm outline-none ${
+                      selected
+                        ? "border-primary-base dark:border-primary-light"
+                        : "border-transparent"
+                    }`}
+                  >
+                    Watchlist
+                  </button>
+                )}
+              </Tab>
+            </Tab.List>
+            <Tab.Panels className={"relative overflow-hidden"}>
+              {/* portfolio */}
+              <Tab.Panel
+                className={"items-top flex justify-center overflow-auto pt-20"}
+              >
+                <FiLock className="size-20 opacity-30" />
+              </Tab.Panel>
+              {/* watchlist */}
+              <Tab.Panel
+                className={"absolute inset-0 overflow-auto"}
+              ></Tab.Panel>
+            </Tab.Panels>
+          </div>
+        </Tab.Group>
+      </div>
     </div>
   );
 }
