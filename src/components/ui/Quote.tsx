2f522e0eb091b378b3ae0e0bc819f386ec161486
wisdomose <wisdomose05@gmail.com>
2024-06-04 11:00:42 +0100

    slider quote

--- src/components/ui/Quote.tsx
+++ src/components/ui/Quote.tsx
@@ -11,6 +11,39 @@ export default function Quote({
   className?: HTMLDivElement["className"];
 }) {
   const isPositive = quote.changesPercentage >= 0;
+
+  return (
+    <div
+      className={twMerge(
+        "white-text w-[220px] rounded-md bg-primary-light/10 px-3 py-2 text-sm font-bold text-[#252525] dark:bg-[#191919]",
+        className
+      )}
+    >
+      <div className="flex items-center justify-between gap-3">
+        <p className="">{quote.symbol}</p>
+        <div
+          className={`flex items-center justify-between py-3 ${
+            isPositive
+              ? "text-main-green-light dark:text-main-green-dark"
+              : "text-main-red-light dark:text-main-red-dark"
+          }`}
+        >
+          <p className="">{quote.price}</p>
+          <div className="">
+            {isPositive ? (
+              <TiArrowSortedUp className="size-6" />
+            ) : (
+              <TiArrowSortedDown className="size-6" />
+            )}
+          </div>
+          <p className="">
+            {/* {isPositive && "+"} */}
+            {quote.changesPercentage}%
+          </p>
+        </div>
+      </div>
+    </div>
+  );
   return (
     <div
       className={twMerge(
