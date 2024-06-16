77c3e749bd7ce5935f1b9cfc308dd05febab3372
wisdomose <wisdomose05@gmail.com>
2024-06-16 15:33:24 +0100

    fix: link to contact page

--- src/app/(app)/about-us/page.tsx
+++ src/app/(app)/about-us/page.tsx
@@ -1,5 +1,6 @@
 import { Container } from "@/components/container";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const data = [
   {
@@ -31,8 +32,8 @@ const data = [
 export default function AboutUsPage() {
   return (
     <main>
-      <header className="bg-black/5 dark:bg-white/5 py-24 text-center px-10">
-        <h1 className=" text-center text-5xl md:text-8xl lg:text-9xl font-bold">
+      <header className="bg-black/5 px-10 py-24 text-center dark:bg-white/5">
+        <h1 className=" text-center text-5xl font-bold md:text-8xl lg:text-9xl">
           Where Finance <br /> Meets Tech.
         </h1>
         <p className="mt-10 text-lg md:text-xl">
@@ -45,9 +46,9 @@ export default function AboutUsPage() {
         {data.map((about, index) => (
           <div
             key={about.category.replaceAll(" ", "-")}
-            className={`mt-24 grid gap-10 md:gap-14 grid-rows-[300px,1fr] md:grid-rows-1 items-center ${index % 2 === 0 ? "md:grid-cols-[3fr,2fr]" : "grid-cols-reverse md:grid-cols-[2fr,3fr] "}`}
+            className={`mt-24 grid grid-rows-[300px,1fr] items-center gap-10 md:grid-rows-1 md:gap-14 ${index % 2 === 0 ? "md:grid-cols-[3fr,2fr]" : "grid-cols-reverse md:grid-cols-[2fr,3fr] "}`}
           >
-            <div className="rounded-xl bg-gray-200 h-full xl:min-h-[400px]"></div>
+            <div className="h-full rounded-xl bg-gray-200 xl:min-h-[400px]"></div>
             <div className={`${index % 2 === 0 ? "" : "md:-order-1"}`}>
               <h2 className="text-xl font-medium">{about.category}</h2>
               <h3 className="mb-9 mt-4 text-3xl font-bold">{about.title}</h3>
@@ -72,14 +73,16 @@ export default function AboutUsPage() {
             </p>
           </div>
 
-          <div className="mt-16 flex flex-col md:flex-row max-md:items-center gap-12 rounded-xl bg-black/5 dark:bg-white/5 px-14 py-20">
+          <div className="mt-16 flex flex-col gap-12 rounded-xl bg-black/5 px-14 py-20 max-md:items-center md:flex-row dark:bg-white/5">
             <div className="h-52 w-52 shrink-0 rounded-full bg-black"></div>
 
             <div className="">
-              <p className="text-3xl font-bold max-sm:text-center">About Jimmy Alan</p>
+              <p className="text-3xl font-bold max-sm:text-center">
+                About Jimmy Alan
+              </p>
               <p className="mb-3 max-sm:text-center">founder</p>
 
-              <p className="max-sm:text-center text-xl">
+              <p className="text-xl max-sm:text-center">
                 Jimmy Alan's journey began as a young 15-year-old Iraqi
                 immigrant who escaped the country after the Desert Storm war and
                 moved to America. Coming from a poor family, Jimmy faced
@@ -90,20 +93,27 @@ export default function AboutUsPage() {
         </div>
       </Container>
 
-      <div className="mt-24 bg-black/5 dark:bg-white/5 px-12 py-12">
-        <div className="mx-auto max-w-7xl grid max-md:grid-rows-[300px,1fr] md:grid-cols-2 gap-10 md:gap-14 items-center">
+      <div className="mt-24 bg-black/5 px-12 py-12 dark:bg-white/5">
+        <div className="mx-auto grid max-w-7xl items-center gap-10 max-md:grid-rows-[300px,1fr] md:grid-cols-2 md:gap-14">
           <div className="">
-            <h1 className="text-6xl md:text-7xl lg:text-6xl font-bold">Connect With Us</h1>
+            <h1 className="text-6xl font-bold md:text-7xl lg:text-6xl">
+              Connect With Us
+            </h1>
             <p className="mb-8 mt-3 text-xl">
               We're always here to help and answer any questions you might have.
               Please fill out the form below, and we'll get back to you as soon
               as possible.
             </p>
 
-            <Button>Contact us</Button>
+            <Link
+              href=""
+              className="h-11 px-4 py-2 inline-flex items-center justify-center whitespace-nowrap rounded-md bg-primary-base text-sm font-medium text-white ring-offset-white transition-colors duration-300 hover:bg-primary-base/80 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-gray-950 dark:focus-visible:ring-gray-300"
+            >
+              Contact us
+            </Link>
           </div>
 
-          <div className="bg-black rounded-xl max-md:-order-1 md:h-[400px]"></div>
+          <div className="rounded-xl bg-black max-md:-order-1 md:h-[400px]"></div>
         </div>
       </div>
     </main>
