19f43008639ff666642ae364dcccf4ec6cb6a60b
wisdomose <wisdomose05@gmail.com>
2024-06-16 17:32:09 +0100

    fix: link to contact us page

--- src/app/(app)/about-us/page.tsx
+++ src/app/(app)/about-us/page.tsx
@@ -106,7 +106,7 @@ export default function AboutUsPage() {
             </p>
 
             <Link
-              href=""
+              href="/contact-us"
               className="h-12 px-8 py-2 inline-flex items-center justify-center whitespace-nowrap rounded-md bg-primary-base text-lg md:text-xl font-medium text-white ring-offset-white transition-colors duration-300 hover:bg-primary-base/80 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-gray-950 dark:focus-visible:ring-gray-300"
             >
               Contact us
