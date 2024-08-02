2ceaef1fc6105fe851573dc1c1c873c8196d8312
wisdomose <wisdomose05@gmail.com>
2024-08-02 20:18:28 +0100

    feat: fear and grid

--- src/modules/market/components/StockMarket/FearAndGreed.tsx
+++ src/modules/market/components/StockMarket/FearAndGreed.tsx
@@ -1,25 +1,122 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
 import useFetcher from "@/hooks/useFetcher";
 import moment from "moment";
+import { motion } from "framer-motion";
+import Spinner from "@/components/spinner";
+
+const a = {
+  lastUpdated: {
+    epochUnixSeconds: 1722618853,
+    humanDate: "2024-08-02T17:14:13.000Z",
+  },
+  fgi: {
+    now: {
+      value: 26,
+      valueText: "Fear",
+    },
+    previousClose: {
+      value: 39,
+      valueText: "Fear",
+    },
+    oneWeekAgo: {
+      value: 45,
+      valueText: "Neutral",
+    },
+    oneMonthAgo: {
+      value: 38,
+      valueText: "Fear",
+    },
+    oneYearAgo: {
+      value: 77,
+      valueText: "Extreme Greed",
+    },
+  },
+};
 
 type FearResponse = {
-  name: string;
+  message: string;
   data: {
-    value: string;
-    value_classification: string;
-    timestamp: string;
-    time_until_update: string;
-  }[];
-  metadata: {
-    error: null;
+    lastUpdated: {
+      epochUnixSeconds: number;
+      humanDate: string;
+    };
+    fgi: {
+      now: {
+        value: number;
+        valueText: string;
+      };
+      previousClose: {
+        value: number;
+        valueText: string;
+      };
+      oneWeekAgo: {
+        value: number;
+        valueText: string;
+      };
+      oneMonthAgo: {
+        value: number;
+        valueText: string;
+      };
+      oneYearAgo: {
+        value: number;
+        valueText: string;
+      };
+    };
   };
 };
 
+/**
+ * {
+    "lastUpdated": {
+        "epochUnixSeconds": 1722618853,
+        "humanDate": "2024-08-02T17:14:13.000Z"
+    },
+    "fgi": {
+        "now": {
+            "value": 26,
+            "valueText": "Fear"
+        },
+        "previousClose": {
+            "value": 39,
+            "valueText": "Fear"
+        },
+        "oneWeekAgo": {
+            "value": 45,
+            "valueText": "Neutral"
+        },
+        "oneMonthAgo": {
+            "value": 38,
+            "valueText": "Fear"
+        },
+        "oneYearAgo": {
+            "value": 77,
+            "valueText": "Extreme Greed"
+        }
+    }
+}
+ */
+
+function mapValueToRange(value: number) {
+  if (value >= 0 && value <= 20) {
+    return 20; // Extreme Fear
+  } else if (value >= 21 && value <= 40) {
+    return 40; // Fear
+  } else if (value >= 41 && value <= 60) {
+    return 60; // Neutral
+  } else if (value >= 61 && value <= 80) {
+    return 80; // Greed
+  } else {
+    return 100; // Extreme Greed
+  }
+}
+
 async function getData() {
-  const res = await fetch(`https://api.alternative.me/fng/`);
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/market/fear-greed-index`
+  );
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
 
@@ -33,58 +130,53 @@ async function getData() {
 
 export default function FearAndGreed() {
   const { loading, data, wrapper } = useFetcher<FearResponse>();
-  const markets = ["U.S", "AMERICAS"];
-  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     wrapper(getData);
   }, []);
 
-  const updateSelectedIndex = (num: number) =>
-    setSelectedIndex((state) => (state === num ? -1 : num));
-
-  if (!data) return null;
-
-  const pieData = data.data.map((index) => ({
-    name: index.value_classification,
-    value: Number(index.value),
-  }));
-
   const otherPieData = [
-    { name: "Extreme Fear", value: 100 / 6 },
-    { name: "Fear", value: 100 / 6 },
-    { name: "Extreme Greed", value: 100 / 6 },
+    { name: "Extreme Fear", value: 20 },
+    { name: "Fear", value: 20 },
+    { name: "Neutral", value: 20 },
+    { name: "Greed", value: 20 },
+    { name: "Extreme Greed", value: 20 },
   ];
 
   const COLORS = [
-    "#FF3932",
-    "#F79009",
-    "#60B527",
-    "#038C8C",
-    "#ffffff",
-    "#5048E5",
+    "#FF0000", // Extreme Fear (red)
+    "#FF4500", // Fear (orange-red)
+    "#FFD700", // Neutral (gold)
+    "#32CD32", // Greed (lime green)
+    "#00FF00", // Extreme Greed (green)
   ];
 
   const RADIAN = Math.PI / 180;
-  const cx = 120;
-  const cy = 200;
-  const iR = 60;
-  const oR = 80;
-  const value = pieData[0].value;
-  //   @ts-ignore
-  const needle = (value, data, cx, cy, iR, oR, color) => {
-    let total = 0;
-    //   @ts-ignore
-    pieData.forEach((v) => {
-      total += v.value;
-    });
-    const ang = 180.0 * (1 - value / total);
-    const length = (iR + 2 * oR) / 4;
+
+  const needle = (
+    value: number,
+    cx: number,
+    cy: number,
+    iR: number,
+    oR: number,
+    color: string
+  ) => {
+    const total = 100;
+    const numSegments = 5; // Number of segments in your data
+    const anglePerSegment = 360 / numSegments; // Each segment's angle in the chart
+
+    // Determine which segment the value falls into and find the center angle
+    const segmentIndex = Math.floor(value / (total / numSegments)) + 1;
+    const segmentAngleStart = anglePerSegment * segmentIndex;
+    const ang = 360 - (segmentAngleStart + anglePerSegment / 2); // Center of the segment
+
+    const length = (iR + 2 * oR) / 5;
     const sin = Math.sin(-RADIAN * ang);
     const cos = Math.cos(-RADIAN * ang);
     const r = 5;
-    const x0 = cx + 5;
-    const y0 = cy + 5;
+    const x0 = cx;
+    const y0 = cy;
     const xba = x0 + r * sin;
     const yba = y0 - r * cos;
     const xbb = x0 - r * sin;
@@ -93,14 +185,6 @@ export default function FearAndGreed() {
     const yp = y0 + length * sin;
 
     return [
-      //   <circle
-      //     cx={x0}
-      //     cy={y0}
-      //     r={r/3}
-      //     fill={color}
-      //     stroke="none"
-      //     stroke-width="3"
-      //   />,
       <path
         key="path"
         d={`M${xba} ${yba}L${xbb} ${ybb} L${xp} ${yp} L${xba} ${yba}`}
@@ -113,93 +197,117 @@ export default function FearAndGreed() {
         r={r}
         fill="#ffffff"
         stroke={color}
-        stroke-width="3"
+        strokeWidth="3"
       />,
-      //   <path
-      //     d={`M${xba} ${yba}L${xbb} ${ybb} L${xp} ${yp} L${xba} ${yba}`}
-      //     stroke="#none"
-      //     fill={color}
-      //   />,
     ];
   };
 
-  console.log(data);
-
   return (
     <>
-      <header className="mb-6 flex items-center justify-between">
+      <header className="mb-6">
         <h2 className="white-text text-2xl font-extrabold capitalize">
           Fear & Greed Index
         </h2>
-
-        <div className="flex gap-2">
-          {markets.map((market, index) => (
-            <button
-              key={market}
-              className={`whitespace-nowrap rounded-full px-4 py-1 text-center text-sm capitalize ${
-                selectedIndex === index
-                  ? "bg-primary-base text-white"
-                  : "bg-hover-focus white-text bg-main-gray-200/20 text-[#636363]"
-              }`}
-              onClick={() => updateSelectedIndex(index)}
-            >
-              {market}
-            </button>
-          ))}
-        </div>
       </header>
 
-      <div className="mb-2 flex items-center justify-between">
-        <p className="text-xl font-medium">
-          What Emotion is driving the Marker Right now?
-        </p>
-        <p className="text-sm">
-          Last updated{" "}
-          {moment(new Date(1000 * Number(data.data[0].timestamp))).format(
-            "MMM D [at] h:mm:ss A"
-          )}
-        </p>
-      </div>
-      {/* <Link href="" className="text-main-blue-base dark:text-main-blue-light">
-        Learn more about the index.
-      </Link> */}
+      <motion.div
+        initial="rest"
+        animate={loading ? "show" : "hide"}
+        variants={{
+          rest: {
+            top: -50,
+            opacity: 0,
+          },
+          show: {
+            opacity: 1,
+            y: -20,
+          },
+          hide: {
+            opacity: 0,
+            y: -50,
+          },
+        }}
+        className="flex items-center justify-center"
+      >
+        <Spinner />
+      </motion.div>
 
-      <div className="mt-2 grid grid-cols-2">
-        <div className="h-80 overflow-auto">
-          <ResponsiveContainer width="100%" height="100%" className="!m-0 !p-0">
-            <PieChart className="">
-              <Pie
-                data={[...pieData, ...otherPieData]}
-                cx={cx}
-                cy={cy}
-                innerRadius={iR}
-                outerRadius={oR}
-                fill="#8884d8"
-                paddingAngle={5}
-                dataKey="value"
-              >
-                {[...pieData, ...otherPieData].map((entry, index) => (
-                  <Cell
-                    key={`cell-${index}`}
-                    fill={COLORS[index % COLORS.length]}
-                  />
-                ))}
-              </Pie>
-              {needle(value, data, cx, cy, iR, oR, "#D0D5DD")}
-            </PieChart>
-          </ResponsiveContainer>
+      {data && (
+        <div className="mb-2 flex flex-wrap items-center justify-between gap-2">
+          <p className="text-xl font-medium">
+            What Emotion is driving the Marker Right now?
+          </p>
+          <p className="text-sm">
+            Last updated{" "}
+            {moment(new Date(data.data.lastUpdated.humanDate)).format(
+              "MMM D [at] h:mm:ss A"
+            )}
+          </p>
         </div>
+      )}
 
-        <div className="flex flex-col gap-8">
-          <Stat
-            reading={Number(data.data[0].value)}
-            index="Greed"
-            timeframe={moment(
-              new Date(1000 * Number(data.data[0].timestamp))
-            ).fromNow()}
-          />
+      {data && (
+        <div className="mt-2 grid md:grid-cols-2">
+          <div className="h-80 overflow-auto">
+            <ResponsiveContainer
+              ref={containerRef}
+              width="100%"
+              height="100%"
+              className="!m-0 !p-0"
+            >
+              <PieChart className="">
+                <Pie
+                  data={[...otherPieData]}
+                  cx={(containerRef?.current?.clientWidth ?? 0) / 2}
+                  cy={(containerRef?.current?.clientHeight ?? 0) / 2}
+                  // innerRadius={iR}
+                  innerRadius={(containerRef?.current?.clientHeight ?? 0) * 0.3}
+                  outerRadius={(containerRef?.current?.clientHeight ?? 0) * 0.4}
+                  // outerRadius={oR}
+                  fill="#8884d8"
+                  paddingAngle={0}
+                  dataKey="value"
+                >
+                  {[...otherPieData].map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={COLORS[index]} />
+                  ))}
+                </Pie>
+                {needle(
+                  mapValueToRange(data.data.fgi.now.value),
+                  (containerRef?.current?.clientWidth ?? 0) / 2,
+                  (containerRef?.current?.clientHeight ?? 0) / 2,
+                  (containerRef?.current?.clientHeight ?? 0) * 0.3,
+                  (containerRef?.current?.clientHeight ?? 0) * 0.4,
+                  "#D0D5DD"
+                )}
+              </PieChart>
+            </ResponsiveContainer>
+          </div>
+
+          <div className="flex flex-col gap-8">
+            <Stat
+              reading={Number(data.data.fgi.now.value)}
+              index={data.data.fgi.now.valueText}
+              timeframe="now"
+            />
+            <Stat
+              reading={Number(data.data.fgi.oneWeekAgo.value)}
+              index={data.data.fgi.oneWeekAgo.valueText}
+              timeframe="one week ago"
+            />
+            <Stat
+              reading={Number(data.data.fgi.oneMonthAgo.value)}
+              index={data.data.fgi.oneMonthAgo.valueText}
+              timeframe="one month ago"
+            />
+            <Stat
+              reading={Number(data.data.fgi.oneYearAgo.value)}
+              index={data.data.fgi.oneYearAgo.valueText}
+              timeframe="one year ago"
+            />
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
